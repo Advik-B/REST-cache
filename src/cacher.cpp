b0c47c22327fae87597d312a27a0b6476df68fbf
Advik <advik.b@gmail.com>
2024-01-15 16:16:52 +0530

    Update cacher.cpp

--- src/cacher.cpp
+++ src/cacher.cpp
@@ -7,3 +7,10 @@
 const char* getHelloWorld() {
     return "Hello World!";
 }
+
+nlohmann::json getJson() {
+    nlohmann::json j;
+    j["Hello"] = "World";
+    j["Dump"] = j.dump(4);
+    return j;
+}
